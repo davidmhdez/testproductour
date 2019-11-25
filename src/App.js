132de2833e771aae04e1934bc4a38599982e2335
David <davidmhdez22@gmail.com>
2019-11-25 12:56:46 -0600

    refactor logic to helphero

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
 import page2 from './pages/page2';
-import Tour from 'reactour';
-
 
 import React, { Component } from 'react';
 
@@ -27,8 +25,6 @@ class App extends Component {
     )
   }
   
-  
-
   render() {
     return (
       <BrowserRouter>
@@ -40,64 +36,10 @@ class App extends Component {
           />
         )}/>
         <Route exact path='/page2' component={page2}/>
-      </Switch>
-      <Tour 
-        steps={steps}
-        isOpen={this.state.isOpenTour}
-        onRequestClose={this.closeTour}
-        closeWithMask={false}
-        // disableDotsNavigation={true}
-        // disableInteraction={true}
-        disableKeyboardNavigation={true}
-        showButtons={false}
-        
-        />
+      </Switch>     
     </BrowserRouter>
     );
   }
 }
-
-function setFocus(){
-  // document.getElementById("field").focus
-  alert("it works!!");
-}
-
-const steps = [
-  {
-    selector:'#title',
-    content: 'this is the first step'
-  },
-  {
-    selector:'#field',
-    content:'you need fill this',
-    // action: node =>{
-    //   node.focus();
-    //   setFocus();
-    //   // 
-    // }
-
-    action: () =>{
-      // document.getElementById('field').focus();
-      alert("it works!!");
-    }
-
-    // content: ({goTo}) =>(
-    //   <button onClick={()=>goTo(0)}>Click me</button>
-    // )
-  },
-  {
-    selector:'#btn',
-    content: 'you must click this button',
-    stepInteraction: false
-  },
-  {
-    selector:'#nextPage',
-    content: 'Go to the next page'
-  },
-  {
-    selector:'#title2',
-    content:'hi, we are in other page'
-  }
-];
 
 export default App;
