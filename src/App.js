32e071af33ea3fe62e960a35531c78ffce251107
David <davidmhdez22@gmail.com>
2019-11-26 12:55:39 -0600

    add helphero settings

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
 import page2 from './pages/page2';
-
+import initHelpHero from 'helphero';
 import React, { Component } from 'react';
 
+var hlp = initHelpHero('iDW653GhT52');
 class App extends Component {
 
   state = {
@@ -15,6 +16,7 @@ class App extends Component {
     this.setState({
       isOpenTour: true
     })
+    hlp.startTour('cfRVozUyTIo')    
   }
 
   closeTour = () => {
