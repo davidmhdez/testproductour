de45404261c6a502ff72d85cd80f2be1e8c3c980
David <davidmhdez22@gmail.com>
2019-11-26 13:41:41 -0600

    dd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
 import page2 from './pages/page2';
-import initHelpHero from 'helphero';
+// import initHelpHero from 'helphero';
 import React, { Component } from 'react';
 
 var hlp = initHelpHero('iDW653GhT52');
@@ -16,7 +16,7 @@ class App extends Component {
     this.setState({
       isOpenTour: true
     })
-    hlp.startTour('cfRVozUyTIo')    
+    // hlp.startTour('cfRVozUyTIo')    
   }
 
   closeTour = () => {
@@ -28,7 +28,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    hlp.anonymous()
+    // hlp.anonymous()
   }
   
   render() {
